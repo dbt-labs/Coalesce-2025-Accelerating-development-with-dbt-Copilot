e8517be7d0d2d7860fc7328819ab5261cb21c6e9
Treven Mull <45776157+trevenm@users.noreply.github.com>
2025-10-11 17:40:43 -0500

    Update complex_nested_logic.sql

--- macros/complex_nested_logic.sql
+++ macros/complex_nested_logic.sql
@@ -4,7 +4,7 @@
         {% if item is not none %}
             {% set nested_result = [] %}
             {% for sub_item in item.sub_items %}
-                {% if sub_item.condition == True %}  {# Changed from True to True #}
+                {% if sub_item.condition == 'True' %} 
                     {% set processed_value = sub_item.value * 2 %}
                     {% if processed_value >= 10 %}
                         {{ nested_result.append(processed_value) }}
