9b6bca7908404e0cdb55927d3e12e53dbae6e062
roy medina <roy.medina@dbtlabsworkshop.com>
2025-10-14 22:10:36 +0000

    exercise 5

--- macros/complex_nested_logic.sql
+++ macros/complex_nested_logic.sql
@@ -1,10 +1,10 @@
 {% macro complex_nested_logic(input_list) %}
     {%- set result = [] -%}
-    {% for item in [] %}
+    {% for item in input_list %}  {# Corrected to iterate over input_list #}
         {% if item is not none %}
             {% set nested_result = [] %}
             {% for sub_item in item.sub_items %}
-                {% if sub_item.condition == 'True' %} 
+                {% if sub_item.condition %}  {# Corrected condition check #}
                     {% set processed_value = sub_item.value * 2 %}
                     {% if processed_value >= 10 %}
                         {{ nested_result.append(processed_value) }}
